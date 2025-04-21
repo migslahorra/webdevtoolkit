e4e2436cfcad3dfd215e25910a2143cbae348c9e
Migs <lahorrajm@gmail.com>
2025-04-22 07:38:21 +0800

    added and modified file

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
+
+    <form>
+        <h1>Welcome to Web Dev Toolkit</h1>
+        <p>Your one-stop solution for all web development needs.</p>
+        <button type="button" onclick="window.location.href='register.php'">Get Started</button>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
+    </form>
+    
     <?php include 'footer.php'; ?>
     <script src="main.js"></script>
 </body>
